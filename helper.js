f45ba95c38b8df2ee5b088614ccbd9eadd9f3644
drikin <drikin@gmail.com>
2011-03-17 14:50:21 -0700

    implement notification feature

--- helper.js
+++ helper.js
@@ -15,4 +15,38 @@ function keydown(event) {
     }
 }
 
+function update(event) {
+    var id = getLastUpdateId();
+    if (id > _last_update_id) {
+        _last_update_id = id;
+        var lu = getLastUpdateElement();
+        var img_url = lu.getElementsByClassName("userimg")[0].src;
+        var name = lu.getElementsByClassName("uname")[0].textContent;
+        var status = lu.getElementsByClassName("ustatus")[0].textContent;
+        var notification = webkitNotifications.createNotification(
+            img_url,
+            name,
+            status);
+        notification.ondisplay = function(event) {
+            setTimeout(function(){event.target.cancel();}, 4000);
+        }
+        notification.show();
+    }
+}
+
+function getLastUpdateElement() {
+    var ru = document.getElementById("realupdates");
+    return ru.getElementsByClassName("update")[0];
+}
+
+function getLastUpdateId() {
+    var lu = getLastUpdateElement();
+    return lu.id;
+}
+
+// attach keydown event
 document.addEventListener("keydown", keydown, false);
+
+// attach DOMNodeInserted event
+var _last_update_id = getLastUpdateId();
+document.addEventListener("DOMNodeInserted", update, false);
