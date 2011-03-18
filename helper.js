ef756095e5cf24a745114759cd888fe7eafb431c
drikin <drikin@gmail.com>
2011-03-17 18:56:03 -0700

    implement unread counter in document.title

--- helper.js
+++ helper.js
@@ -19,18 +19,27 @@ function update(event) {
     var id = getLastUpdateId();
     if (id > _last_update_id) {
         _last_update_id = id;
+
+        // update window title
+        _unread_count++;
+        document.title = "(" + _unread_count + ") " + _title;
+
+        // show notification
         var lu = getLastUpdateElement();
         var img_url = lu.getElementsByClassName("userimg")[0].src;
         var name = lu.getElementsByClassName("uname")[0].textContent;
         var status = lu.getElementsByClassName("ustatus")[0].textContent;
-        var notification = webkitNotifications.createNotification(
-            img_url,
-            name,
-            status);
-        notification.ondisplay = function(event) {
-            setTimeout(function(){event.target.cancel();}, 4000);
+        try {
+            var notification = webkitNotifications.createNotification(
+                img_url,
+                name,
+                status);
+            notification.ondisplay = function(event) {
+                setTimeout(function(){event.target.cancel();}, 4000);
+            }
+            notification.show();
+        } catch (e) {
         }
-        notification.show();
     }
 }
 
@@ -44,9 +53,11 @@ function getLastUpdateId() {
     return lu.id;
 }
 
-// attach keydown event
-document.addEventListener("keydown", keydown, false);
-
-// attach DOMNodeInserted event
+var _title = document.title;
 var _last_update_id = getLastUpdateId();
+var _unread_count = 0;
+
+// attach events
+document.addEventListener("keydown", keydown, false);
 document.addEventListener("DOMNodeInserted", update, false);
+
